d2060163e12e7674eb16b3619e8da0720c8b356e
Vamses <108464869+VamsesOfficial@users.noreply.github.com>
2022-10-21 21:18:49 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -6,22 +6,20 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let str = `${global.wm}
 
 ┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
+│ • *INDOSAT:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
 │ • *Dana:* [${global.pdana}]
-│ • *Gopay:* [${global.pgopay}]
 │ • *Ovo:* [${global.povo}]
-│ • *Link Aja:* [${global.plinkaja}]
 ❏────`
-let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
+let wibu = `https://telegra.ph/file/bc3547baa36be4dea58d8.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/ketutagus404",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://www.instagram.com/p/Cj0Nv8bPgvi/?igshid=YmMyMTA2M2Y=", 
+    title: 'VAMSES MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig
